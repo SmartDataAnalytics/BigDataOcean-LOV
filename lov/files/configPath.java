abfc88a088551e623d0baf34a7271a0813671bf5
ilytra <ioanna.lytra@cs.uni-bonn.de>
2017-06-23 12:13:29 +0200

    initial commit

---
+++ lov/files/configPath.java
@@ -0,0 +1,10 @@
+package org.lov.cli;
+
+/**
+ * Absolute path for the configuration file lov.config
+ * 
+ */
+
+public class configPath {
+	public static String configFilePath="/BDO/lovScripts/lov.config";
+}
---
+++ lov/files/configPath.js
@@ -0,0 +1,9 @@
+/**
+ * Global variables in:
+ express.js 
+ bot.js 
+ versions.js 
+ vocabularies.js.
+ */
+exports.path = '/BDO';
+exports.URL = 'http://localhost:3333/dataset/bdo';
