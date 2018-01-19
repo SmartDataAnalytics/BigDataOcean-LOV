db32941c0ecc5b28197219123396720f900e41c5
Jaime M Trillos <jtrillos@users.noreply.github.com>
2018-01-19 18:53:21 +0100

    Update initLOV.sh

--- initLOV.sh
+++ initLOV.sh
@@ -12,6 +12,9 @@ docker exec -it bigdataoceanlov_lov_1 bash -c 'cd /BDO/lov/setup/BDO_setup;
     mongoimport -h mongodb -d bdo -c pilots --file pilots.json'
 
 # Update elasticsearch index
-# It runs every day (86400 seconds) to update the bdo.n3 and bdo.nq and the elasticsearch index
+# It runs every day (86400 seconds) to update the bdo.n3 and bdo.nq and the elasticsearch index and last the endpoint data is updated
 docker exec -it bigdataoceanlov_lov_1 bash -c 'cd /BDO/lovScripts/target/lovscripts-cli/lovscripts/bin/;
-    while true; do ./mongo2rdf && ./create-index && ./index-lov; sleep 86400; done'
+    while true; do ./mongo2rdf && ./create-index && ./index-lov; 
+    cd /BDO/; cd ..; 
+    /apache-jena-fuseki-3.4.0/bin/s-put http://fuseki:3030/bigdataocean/data default BDO/lov/public/lov.n3
+    sleep 86400; done'
