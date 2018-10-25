6e26becfe555eeebf7e4402dedbe8410ba0c1867
jtrillos <jaimemanueltrillos@gmail.com>
2018-10-25 12:27:05 +0200

    adding updateLOV.sh

---
+++ updateLOV.sh
@@ -0,0 +1,5 @@
+# Update the elasticsearch data when new data is inserted in the system
+docker exec -it bigdataoceanlov_lov_1 bash -c 'cd /BDO/lovScripts/target/lovscripts-cli/lovscripts/bin/;
+    ./mongo2rdf && ./create-index && ./index-lov; 
+    cd /BDO/; cd ..; 
+    /apache-jena-fuseki-3.4.0/bin/s-put http://fuseki:3030/bigdataocean/data default BDO/lov/public/lov.n3'
